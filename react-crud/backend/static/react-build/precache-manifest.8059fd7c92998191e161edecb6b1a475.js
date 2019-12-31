20b25e0f7a2461abac5011c3fed5e08497e9ac32
Tiago Bockholt <latiagobockholt@Tiagos-MacBook-Pro.local>
2019-12-31 02:29:18 -0300

    final solution that runs flask and React

---
+++ react-crud/backend/static/react-build/precache-manifest.8059fd7c92998191e161edecb6b1a475.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c1b11e362200f95571f3477b2c8452",
+    "url": "/static/react-build/../../templates/index.html"
+  },
+  {
+    "revision": "5b7ee4085cc3baffb831",
+    "url": "/static/react-build/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "31d605b83920e1cdcbb0",
+    "url": "/static/react-build/js/2.dd1fcc93.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/react-build/js/2.dd1fcc93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b7ee4085cc3baffb831",
+    "url": "/static/react-build/js/main.60e48a30.chunk.js"
+  },
+  {
+    "revision": "3db874264f63e534906d",
+    "url": "/static/react-build/js/runtime-main.37290c0f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react-build/media/logo.5d5d9eef.svg"
+  }
+]);
